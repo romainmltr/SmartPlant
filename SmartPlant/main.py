885dbd0ffe2e3d59c97db112bdb70482ec3ebce8
romainmltr <romain.malaterre@hotmail.fr>
2023-03-03 16:57:43 +0100

    Push public

--- SmartPlant/main.py
+++ SmartPlant/main.py
@@ -3,31 +3,56 @@ import network   #import des fonctions lier au wifi
 import urequests    #import des fonction lier au requetes http
 import utime    #import des fonction lier au temps
 import ujson    #import des fonction lier aà la convertion en Json
-adc_pin = machine.ADC(28)
-relay_pin = machine.Pin(16, machine.Pin.OUT)
+
+sensor = machine.ADC(28)
+relay = machine.Pin(16, machine.Pin.OUT)
+led = machine.Pin(17, machine.Pin.OUT)
+led.off()
 
 wlan = network.WLAN(network.STA_IF) # met la raspi en mode client wifi
 wlan.active(True) # active le mode client wifi
 
 ssid = 'iPhone de Quentin'
-password = 'N1c0l4s14!'
+password = 'quentinco'
 wlan.connect(ssid, password) # connecte la raspi au réseau
 url = "http://172.20.10.3:3000/humidity"
+url_water = "http://172.20.10.3:3000/water"
+
+def pump_on():
+    led.on()
+    relay.value(1)
+    utime.sleep(2)
+    #print("Pump On")
+
+def pump_off():
+    led.off()
+    relay.value(0)
+    #print("Pump Off")
+
+def handle_button_press():
+    response = urequests.get(url_water)
+    json = response.json()
+    water = json['water']
+    response.close()
+    if water == 0:
+        pump_off()
+    else:
+        pump_on()
 
 while not wlan.isconnected(): # tant que la raspi n'est pas connecté
     print("connecting...")
-    utime.sleep(3) # attendre 3 secondes
+    utime.sleep(1)
 
 while (True):
-    adc_val = adc_pin.read_u16()
-    try:
-        print("POST")
-        # lance une requete sur l'url
-        r = urequests.post(url, json={"value": adc_val})
+    handle_button_press()
 
-        print("sucess")
-        r.close() # ferme la connexion
-        utime.sleep(5)
+    sensor_val = sensor.read_u16()
+    try:
+        #print("POST value")
+        print(sensor_val)
+        r = urequests.post(url, json={"value": sensor_val})
+        r.close()
+        utime.sleep(2)
 
     except Exception as e:
         print(e)
